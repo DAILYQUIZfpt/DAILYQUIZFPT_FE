b96579cbdd7cf54422e41366428593bcf55b4679
tthienduc13 <ducnltdev@gmail.com>
2023-12-06 19:59:06 +0700

    setup redux

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { siteConfig } from "@/config/site";
 import TanstackProvider from "@/components/providers/TanstackProvider";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -27,7 +29,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <TanstackProvider>{children}</TanstackProvider>
+        <Provider store={store}>
+          <TanstackProvider>{children}</TanstackProvider>
+        </Provider>
       </body>
     </html>
   );
